abb17e0cde25cb494b207f95fea0255acafbcde0
Gabe Harvey <harvey7@gmail.com>
2024-04-28 14:01:03 -0500

    Added Mutation Type to schemas/typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,14 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
-type: Query {
+type Query {
     me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SaveBookInput): User
+    removeBook(bookId: String!): User
 }
 `
 
