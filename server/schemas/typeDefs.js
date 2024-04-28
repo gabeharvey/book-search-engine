4036f829ffaf7bd90d1e931b84f4481f696ec23b
Gabe Harvey <harvey7@gmail.com>
2024-04-28 14:13:26 -0500

    Added Book Type to schemas/typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,6 +17,14 @@ type User {
     bookCount: Int
     savedBooks: [Book]
 }
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
 `
 
 // Export typeDefs
