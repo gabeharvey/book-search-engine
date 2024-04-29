13f670a70fae583ac273342ce80f0f62db652386
Gabe Harvey <harvey7@gmail.com>
2024-04-29 02:22:35 -0500

    Added Apollo Client Instance and GraphQL Server Connection to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,26 @@ import SavedBooks from './pages/SavedBooks';
 import {setContext} from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
 
+// This Establishes Connection to GraphQL Server
+const webLink = createHttpLink({uri: '/graphql'});
+
+// This Establishes Authorization Headers
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// This Creates Apollo Client Instance
+const client = new ApolloClient({
+  link: authLink.concat(webLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <>
