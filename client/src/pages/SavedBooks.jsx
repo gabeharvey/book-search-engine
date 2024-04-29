bca262f505300c93aeb8350543e581d452462860
Gabe Harvey <harvey7@gmail.com>
2024-04-29 11:48:30 -0500

    Updated Imports on pages/SavedBooks.jsx and pages/SearchBooks.jsx

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import {
   Container,
   Card,
@@ -7,7 +7,9 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import {GET_ME} from '../utils/queries';
+import {REMOVE_BOOK} from '../utils/mutations';
+import {useQuery, useMutation} from '@apollo/react-hooks';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Col,
@@ -26,8 +26,11 @@ const SearchBooks = () => {
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+    return () => {
+      saveBookIds(savedBookIds);
+    };
+  }, [savedBookIds]);
+
   const [saveBook] = useMutation(SAVE_BOOK);
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
