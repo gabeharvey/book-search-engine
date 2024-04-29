ccc2a5d19c263d6bee2a477d2ae97599d5f0c3cc
Gabe Harvey <harvey7@gmail.com>
2024-04-29 01:51:16 -0500

    Added Required Imports to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
+// Required Imports for Application
+import React from 'react';
+import auth from './utils/auth';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import {setContext} from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
 
 function App() {
