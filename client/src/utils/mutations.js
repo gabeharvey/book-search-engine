a8b570b028c0776d97c6583a42763f991bdebb84
Gabe Harvey <harvey7@gmail.com>
2024-04-28 23:33:02 -0500

    Added LoginUser and AddUser Mutations to utils/mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,29 @@
 // This Imports GQL Function from Apollo Client
 import {gql} from '@apollo/client';
+
+// This Executes Login User Mutation to Verify User
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+// This Executes Add User Mutation to Create a New User
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password:$password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
