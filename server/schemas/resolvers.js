4ea0f5d27323ae3013914276b6522d7086f32aa1
Gabe Harvey <harvey7@gmail.com>
2024-04-28 19:27:05 -0500

    Added Create User and Login Resolver Functions to schemas/resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,30 @@ const {User}= require('../models');
 
 const resolvers = {
     Query: {
-        // This Function Checks if User is Logged In
+        // This Function Fetches User Data if User is Logged In
         me: async(parents, args, context) => {
             if(!context.user){
                 throw new AuthenticationError('User Not Logged In')
             }
             const userData = await User.findById(context.user._id).select('-__v -password');
             return userData;
-        }
+        },
+    },
+    Mutation: {
+        // This Function Allows User to Log In
+        login: async(parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if(!user || !(await user.isCorrectPassword(password))) {
+                throw new AuthenticationError('Email or Password is Invalid');
+            }
+            const token = signToken(user);
+            return {token, user}
+        },
+        // This Function Allows Creation of New User
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        },
     }
 }
