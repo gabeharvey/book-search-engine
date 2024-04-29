a432fc77bccbdbcd0c768b9743f710a4f623fa37
Gabe Harvey <harvey7@gmail.com>
2024-04-28 23:18:01 -0500

    Defined GraphQL Operation in utils/queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,21 @@
 // This Imports GQL Function from Apollo Client
 import {gql} from '@apollo/client';
+
+// This Defines GraphQL Operation Required for Application
+export const GET_ME = gql`
+    query {
+        me {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
+`;
