40a2979698a9a1a273ea9f284e9d1e1f1baf2462
Gabe Harvey <harvey7@gmail.com>
2024-04-29 11:29:51 -0500

    Added Fetch API Data Request and Imports to /pages/SearchBooks.jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -11,6 +11,8 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {SAVE_BOOK} from '../utils/mutations';
+import {useMutation} from '@apollo/react-hooks';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -26,7 +28,7 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
-
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -36,8 +38,8 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
-
+      // Fetch Book Data from API
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
